5912340d0e652fb9cc3f6fbba5122f82e63292e2
cristianmoise <moise.cristian20@gmail.com>
2024-05-21 21:30:41 +0300

    making a script to convert json to xlsx

---
+++ convert_json_to_xlsx.py
@@ -0,0 +1,18 @@
+import json
+import pandas as pd
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+records = []
+
+for line in data['lines']:
+
+    combined_data = {**data, **line}
+    # Excludem cheile care nu sunt necesare din nivelul principal (cum ar fi 'lines')
+    combined_data.pop('lines', None)
+    records.append(combined_data)
+
+df = pd.DataFrame(records)
+
+df.to_excel('output.xlsx', index=False)
